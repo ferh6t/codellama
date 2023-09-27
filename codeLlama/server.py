15d586924ef017a20eae6a0b292f49ca62bbba4b
ferh6t <karacaferhat6@gmail.com>
2023-09-27 14:49:13 -0700

    deleted generator

--- codeLlama/server.py
+++ codeLlama/server.py
@@ -60,22 +60,7 @@ def createCodeGenerator(
         max_seq_len=max_seq_len,
         max_batch_size=max_batch_size,
     )
-def createChatGenerator(
-    ckpt_dir: str,
-    tokenizer_path: str,
-    temperature: float = 0.6,
-    top_p: float = 0.9,
-    max_seq_len: int = 512,
-    max_batch_size: int = 8,
-    max_gen_len: Optional[int] = None,
-):
-    global text_generator
-    text_generator = Llama.build(
-        ckpt_dir=ckpt_dir,
-        tokenizer_path=tokenizer_path,
-        max_seq_len=max_seq_len,
-        max_batch_size=max_batch_size,
-    )
+
     
 # Define your PyTorch method
 if __name__ == "__main__":
