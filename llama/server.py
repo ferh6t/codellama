2b39d85cf075697365c0815c71f30bd618403683
ferh6t <karacaferhat6@gmail.com>
2023-09-29 07:43:19 -0700

    as

--- llama/server.py
+++ llama/server.py
@@ -20,7 +20,7 @@ You are a helpful, respectful and honest assistant. Always answer as helpfully a
 If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.""",
             },
             {"role": "user", "content": "Write a brief birthday message to John"}, 
-        ]
+        ],
 #             {
 #                 "role": "system",
 #                 "content": """
