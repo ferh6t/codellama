473f3b2fe5176d96df94c39fb8efb51135d427f7
ferh6t <karacaferhat6@gmail.com>
2023-09-29 07:57:26 -0700

    init

--- llama/server.py
+++ llama/server.py
@@ -11,16 +11,7 @@ top_p: float = 0.9
 
 def askChatModel(data):
     dialogs: List[Dialog] = [
-        [
-            {
-                "role": "system",
-                "content": """\
-You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe. Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.
-
-If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.""",
-            },
-            {"role": "user", "content": "Write a brief birthday message to John"}, 
-        ],       
+        [{"role": "user", "content": data}]        
     ]
     results = generator.chat_completion(
         dialogs,
