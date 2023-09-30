74da86151858f2e40700e93f5095dbd1d9762f35
ferh6t <karacaferhat6@gmail.com>
2023-09-30 04:31:57 -0700

    example of john

--- llama/server.py
+++ llama/server.py
@@ -14,9 +14,12 @@ def askChatModel(data):
         [
             {
                 "role": "system",
-                "content": "Always answer with emojis",
+                "content": """\
+You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe. Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.
+
+If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.""",
             },
-            {"role": "user", "content": "How to go from Beijing to NY?"},
+            {"role": "user", "content": "Write a brief birthday message to John"},
         ]        
     ]
     results = generator.chat_completion(
